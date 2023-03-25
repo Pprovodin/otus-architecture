8067015b02858877c0349cc766f0eb68f156a970
Pavel Provodin <provodin-pavel@mail.ru>
2023-03-25 17:17:22 +0300

    01-module-test-homework | fix little bug

--- 01-module-tests/src/main/java/ru/otus/quadraticequation/Solver.java
+++ 01-module-tests/src/main/java/ru/otus/quadraticequation/Solver.java
@@ -10,8 +10,8 @@ public class Solver {
 
         double discriminant = Math.pow(b, 2) - 4 * a * c;
 
-        if (discriminant < 0) return new double[]{};
         if (isZero(discriminant)) return new double[] {(-1) * b / 2 / a};
+        if (discriminant < 0) return new double[]{};
         return new double[] {((-1) * b + Math.sqrt(discriminant)) / 2 / a,
                 ((-1) * b - Math.sqrt(discriminant)) / 2 / a};
     }
